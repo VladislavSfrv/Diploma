8b3737d589a0090c46afb57b860f197e9152af83
Vladislav <vladislavsafarov04@mail.ru>
2025-03-09 14:19:27 +0300

    Menu

--- src/MenuNavigator/MenuNavigator.jsx
+++ src/MenuNavigator/MenuNavigator.jsx
@@ -17,8 +17,8 @@ function MenuNavigator() {
             <ul className="menu__list">
                 <li className="menu__list-item"><img src="/src/assets/icons/key-square.svg" alt="key" /> <span className="menu__list-item__text">Dashboard</span></li>
                 <li className="menu__list-item"><img src="/src/assets/icons/d-square.svg" alt="" /> <span className="menu__list-item__text">Product</span> <span className="arrow-right">{`>`}</span></li>
-                <li className="menu__list-item menu__list-item-active"><img src="/src/assets/icons/user-square.svg" alt="" /> <span className="menu__list-item__text">Customers</span> <span className="arrow-right">{`>`}</span></li>
-                <li className="menu__list-item"><img src="/src/assets/icons/wallet-square.svg" alt="" /> <span className="menu__list-item__text">Income</span> <span className="arrow-right">{`>`}</span></li>
+                <li className="menu__list-item"><img src="/src/assets/icons/user-square.svg" alt="" /> <span className="menu__list-item__text">Customers</span> <span className="arrow-right">{`>`}</span></li>
+                <li className="menu__list-item menu__list-item-active"><img src="/src/assets/icons/wallet-square.svg" alt="" /> <span className="menu__list-item__text">Income</span> <span className="arrow-right">{`>`}</span></li>
                 <li className="menu__list-item"><img src="/src/assets/icons/discount-square.svg" alt="" /> <span className="menu__list-item__text">Promote</span> <span className="arrow-right">{`>`}</span></li>
                 <li className="menu__list-item"><img src="/src/assets/icons/help-square.svg" alt="" /> <span className="menu__list-item__text">Help</span> <span className="arrow-right">{`>`}</span></li>
             </ul>
@@ -29,12 +29,12 @@ function MenuNavigator() {
             </div>
 
             <div className="menu__profile">
-                <div className="menu__profile-photo"></div>
-                <div className="menu_profile-text">
-                    <h3 className="menu__profile-name">Evano</h3>
-                    <p className="menu__profile-special">Project Manager</p>
-                </div>
-                <span className="arrow-down"></span>
+                <img src="/src/assets/icons/avatar.svg" alt="avatar" />
+                <span className="menu__profile-text">
+                    <h3 className="menu__profile-text__name">Evano</h3>
+                    <p className="menu__profile-text__special">Project Manager</p>
+                </span>
+                <span className="menu__profile-arrow-down"><img src="/src/assets/icons/arrow-down.svg" alt="arrow down" /></span>
             </div>
         </nav>
     )
